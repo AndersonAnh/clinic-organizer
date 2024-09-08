08ea0eb51e6cfc3da37f09a29ff01387cb3ac8f9
NMWXask <k.marcus@mail.ru>
2024-09-08 14:24:46 +0400

    Initial commit

---
+++ src/main/java/ru/clinic/org/clinicorganizer/ClinicOrganizerApplication.java
@@ -0,0 +1,13 @@
+package ru.clinic.org.clinicorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicOrganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/clinic/org/clinicorganizer/ClinicOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clinic.org.clinicorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
