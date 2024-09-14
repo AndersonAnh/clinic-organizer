e566ef6af615c03aba7afc96c0c4968eced65cfc
Андрей <ery-palmbeach@mail.ru>
2024-09-14 18:49:07 +0300

    CLO-2 Правки

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -5,19 +5,22 @@ CREATE TABLE project.doctors(
     first_name VARCHAR(128) NOT NULL,
     last_name VARCHAR(128) NOT NULL,
     specialization_id INT,
-    FOREIGN KEY (specialization_id) references project.specializations(id)
+    FOREIGN KEY (specialization_id) REFERENCES project.specializations(id)
 );
+
 CREATE TABLE project.specializations(
     id INT PRIMARY KEY ,
     name VARCHAR(50) NOT NULL
 );
+
 CREATE TABLE project.patients(
     id INT PRIMARY KEY ,
     first_name VARCHAR(128) NOT NULL,
     last_name VARCHAR(128) NOT NULL,
     doctor_id INT,
-    FOREIGN KEY (doctor_id) references project.doctors(id)
+    FOREIGN KEY (doctor_id) REFERENCES project.doctors(id)
 );
+
 DROP TABLE project.doctors;
 DROP TABLE project.specializations;
 DROP TABLE project.patients;
