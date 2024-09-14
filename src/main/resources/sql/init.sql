dfec80d5066b8593bd82a70e2af7a14ee818c879
Андрей <ery-palmbeach@mail.ru>
2024-09-14 18:31:28 +0300

    CLO-2 Спроектировал таблицы в БД

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,1 +1,23 @@
 CREATE SCHEMA project;
+
+CREATE TABLE project.doctors(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(128) NOT NULL,
+    last_name VARCHAR(128) NOT NULL,
+    specialization_id INT,
+    FOREIGN KEY (specialization_id) references project.specializations(id)
+);
+CREATE TABLE project.specializations(
+    id INT PRIMARY KEY ,
+    name VARCHAR(50) NOT NULL
+);
+CREATE TABLE project.patients(
+    id INT PRIMARY KEY ,
+    first_name VARCHAR(128) NOT NULL,
+    last_name VARCHAR(128) NOT NULL,
+    doctor_id INT,
+    FOREIGN KEY (doctor_id) references project.doctors(id)
+);
+DROP TABLE project.doctors;
+DROP TABLE project.specializations;
+DROP TABLE project.patients;
